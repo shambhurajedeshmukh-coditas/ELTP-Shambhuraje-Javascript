3d0596e1d374ebabfd3e35b1d4d833d0662a3e90
shambhurajedeshmukh-coditas <158143367+shambhurajedeshmukh-coditas@users.noreply.github.com>
2024-01-30 14:50:59 +0530

    JavaScript day2 assignment solution

---
+++ class-assignment-day-two.js
@@ -0,0 +1,31 @@
+//Declared four variables without assigning values
+let accountNumber;
+let accountBalance;
+let withdrawLimit;
+let accountHolderName;
+
+//Declared four variables with assigned values
+const voterID=234587;
+const voterName='Sam';
+const voterAge=21;
+const votingPlace='Pune';
+
+//Declared variables to store your first name, last name, marital status, country and age in multiple lines
+
+/*
+
+let firstName='Shambhuraje';
+let lastName='Deshmukh';
+let maritalStatus='Single';
+let countryName='India';
+let age=21;
+
+*/
+
+//Declared variables to store your first name, last name, marital status, country and age in a single line
+let firstName='Sanket',lastName='Joshi',maritalStatus='Single',countryName='India',age=21;
+
+//Declared two variables myAge and yourAge and assign them initial values and log to the browser console.
+let myAge=21;
+let yourAge=28;
+console.log(`myAge is ${myAge} and yourAge is ${yourAge}`);
