fcd012423bf1803ed93c3446d8326cacf488f53d
shambhurajedeshmukh-coditas <158143367+shambhurajedeshmukh-coditas@users.noreply.github.com>
2024-01-31 13:37:42 +0530

    JS assignment 3 solution

---
+++ class-assignment-day-3.js
@@ -0,0 +1,11 @@
+//created the four diffrent variables
+const myName='Coditas';
+const isEmployee=true;
+const mySalary=undefined;
+const myPosition=null;
+
+//by using the typeOf keyword we can log the data type
+console.log(typeof(myName));//string
+console.log(typeof(isEmployee))//Boolean
+console.log(typeof(mySalary))//undefined
+console.log(typeof(myPosition));//object
