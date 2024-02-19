08f63e7f7b4c187c02312f2830a78c8c50d7a951
shambhurajedeshmukh-coditas <158143367+shambhurajedeshmukh-coditas@users.noreply.github.com>
2024-02-19 11:20:46 +0530

    pattern in js

---
+++ pattern.js
@@ -0,0 +1,10 @@
+for(let counter=0;counter<5;counter++){
+    for(let index=counter+2;index<5;index++){
+        console.log("\n");
+        console.log("+")
+         
+    }
+    
+
+}
+console.log('\t')
